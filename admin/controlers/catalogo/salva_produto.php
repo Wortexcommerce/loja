185a62881e9d384e22f99c25123cddf8e45f002c
Emilio Emidio <contato@sisconnection.com.br>
2018-05-21 15:33:39 -0300

    emilio

--- admin/controlers/catalogo/salva_produto.php
+++ admin/controlers/catalogo/salva_produto.php
@@ -2,7 +2,48 @@
 require("../../config.php");
 
 
-$sql = $db->select("INSERT INTO cad_produtos () VALUES ()");
+$sql = $db->select("INSERT INTO cad_produtos (  nome_produto,
+												sku_produto,
+												resumo_produto,
+												preco_normal_produto,
+												preco_promocional_produto,
+												preco_custo_produto,
+												descricao_produto,
+												gerenciar_produto,
+												quantidade_fixa_produto,
+												quantidade_produto,
+												ean_produto,
+												ncm_produto,
+												peso_produto,
+												comprimento_produto,
+												largura_produto,
+												altura_produto,
+												url_pagina_produto,
+												titulo_pagina_produto,
+												descricao_pagina_produto,
+												palavras_chave_produto
+												) VALUES (
+												'$nome_produto',
+												'$sku_produto',
+												'$resumo_produto',
+												'$preco_normal_produto',
+												'$preco_promocional_produto',
+												'$preco_custo_produto',
+												'$descricao_produto',
+												'$gerenciar_produto',
+												'$quantidade_fixa_produto',
+												'$quantidade_produto',
+												'$ean_produto',
+												'$ncm_produto',
+												'$peso_produto',
+												'$comprimento_produto',
+												'$largura_produto',
+												'$altura_produto',
+												'$url_pagina_produto',
+												'$titulo_pagina_produto',
+												'$descricao_pagina_produto',
+												'$palavras_chave_produto'
+											)");
  
 
 //SESSIONS DE AVISO//
